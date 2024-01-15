696c2d78256780b1c5e8c178303775f18d1a2232
tamaranaj <najdovska18@gmail.com>
2024-01-15 23:06:10 +0100

    Homework

--- Homeworks-BasicJS/20231223/src/homeworkEx2.js
+++ Homeworks-BasicJS/20231223/src/homeworkEx2.js
@@ -1,35 +1,59 @@
 //Write a JavaScript program to display the reading status of some book. The object should have the next properties: title, author, readingStatus and a method that will return info depending on the readingStatus e.g. Already read 'The Robots of dawn' by Isaac Asimov. (if true)You still need to read 'Mockingjay: The Final Book of The Hunger Games' by Suzanne Collins. (if false).
 // BONUS: ENTER THE VALUES FROM PROMPT() OR READ THEM FROM HTML
 
-
+//first way
 let nameOfBook = prompt("Enter name of some book")
 let nameOfAuthor = prompt("Enter the name of author")
 let bookStatus = confirm("Is this book read?")
 
 
-if(!nameOfBook||!nameOfAuthor){        
-    console.log("error")
-}else{
-
-    const book = {
-    
-        title: nameOfBook,
-        author: nameOfAuthor,
-        readingStatus: bookStatus,
-    
-        readOrNot : function(){
-            if(this.readingStatus){
-                console.log(`Already read ${this.title} from ${this.author}`)
-            }else{
-                console.log(`You still need to read ${this.title} from ${this.author}`)
-            }
-    
-            
+function Book (name, author, readingBook){
+    this.name = name;
+    this.author = author;
+    this.readingBook = readingBook;
+    this.readingStatus = function(){
+        if(this.readingBook){
+            console.log(`Already read ${this.name} from ${this.author}`)
+        }else{
+            console.log(`You still need to read ${this.name} from ${this.author}`)
         }
     }
-    
-    book.readOrNot();
+}
 
+function creatingBookProgram(){
+    if(!nameOfBook||!nameOfAuthor){        
+        console.log("Enter inputs")
+        return
+    }
+    let result = new Book(nameOfBook,nameOfAuthor,bookStatus)
+
+    console.log(result.readingStatus())
 
 }
+creatingBookProgram()
+
+
+
+//second way
+
+// const book = {
+//     title: nameOfBook,
+//     author: nameOfAuthor,
+//     readingStatus: bookStatus,
+    
+//     readOrNot : function(){
+//         if(this.readingStatus){
+//             console.log(`Already read ${this.title} from ${this.author}`)
+//         }else{
+//             console.log(`You still need to read ${this.title} from ${this.author}`)
+//         }
+    
+            
+//     }
+// }
+    
+// book.readOrNot();
+
+
+
 
